fbffc12ff2052d522ce0f1a45e6adfc97ba27547
Yosua Eben Haezer Momongan <215314148@student.usd.ac.id>
2026-05-08 14:19:43 +0700

    fix App.jsx -delete router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainLayout from './components/layout/MainLayout';
-import AdminLayout from './components/layout/AdminLayout';
-import LandingPage from './pages/LandingPage';
-import Vehicles from './pages/Vehicles';
-import VehicleDetail from './pages/VehicleDetail';
-import AdminDashboard from './pages/AdminDashboard';
+import { Routes, Route } from "react-router-dom";
+import Main from "./components/layout/AdminLayout"; import MainLayout from "./components/layout/MainLayout";
+import LandingPage from "./pages/LandingPage";
+import Vehicles from "./pages/Vehicles";
+import VehicleDetail from "./pages/VehicleDetail";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public Routes with MainLayout */}
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<LandingPage />} />
-          <Route path="fleet" element={<Vehicles />} />
-          <Route path="fleet/:id" element={<VehicleDetail />} />
-        </Route>
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<LandingPage />} />
+        <Route path="fleet" element={<Vehicles />} />
+        <Route path="fleet/:id" element={<VehicleDetail />} />
+      </Route>
 
-        {/* Admin Routes with AdminLayout */}
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<AdminDashboard />} />
-          <Route path="bookings" element={<AdminDashboard />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      {/* Admin Routes */}
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<AdminDashboard />} />
+        <Route path="bookings" element={<AdminDashboard />} />
+      </Route>
+    </Routes>
   );
 }
 
