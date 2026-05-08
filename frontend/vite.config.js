5dd1264403e5ee75bc07fef01d8860f5aaad7e5e
Yosua Eben Haezer Momongan <215314148@student.usd.ac.id>
2026-05-08 13:47:59 +0700

    fix github pages config and deploy scripts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Rental-Kendaraan/',
+  base: '/Rental_Kendaraan/',
   plugins: [react()],
 })
 
